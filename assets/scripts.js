4d4ec956f895a7a6626d7d2a6466371c99de46d3
Gerard Hynes <gkhynes@gmail.com>
2018-05-17 21:52:40 +0100

    Add smooth scrolling

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,16 +1,43 @@
-(function() {
-  "use strict";
+// (function() {
+//   "use strict";
 
-  var section = document.querySelectorAll(".section");
-  var sections = {};
-  var i = 0;
+//   var section = document.querySelectorAll(".section");
+//   var sections = {};
+//   var i = 0;
 
-  Array.prototype.forEach.call(section, function(e) {
-    sections[e.id] = e.offsetTop;
-  });
+//   Array.prototype.forEach.call(section, function(e) {
+//     sections[e.id] = e.offsetTop;
+//   });
 
-  window.onscroll = function() {
-    var scrollPosition =
-      document.documentElement.scrollTop || document.body.scrollTop;
-  };
-})();
+//   window.onscroll = function() {
+//     var scrollPosition =
+//       document.documentElement.scrollTop || document.body.scrollTop;
+//   };
+// })();
+
+function anchorLinkHandler(e) {
+  const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+
+  e.preventDefault();
+  const targetID = this.getAttribute("href");
+  const targetAnchor = document.querySelector(targetID);
+  if (!targetAnchor) return;
+  const originalTop = distanceToTop(targetAnchor);
+
+  window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" });
+
+  const checkIfDone = setInterval(function() {
+    const atBottom =
+      window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+    if (distanceToTop(targetAnchor) === 0 || atBottom) {
+      targetAnchor.tabIndex = "-1";
+      targetAnchor.focus();
+      window.history.pushState("", "", targetID);
+      clearInterval(checkIfDone);
+    }
+  }, 100);
+}
+
+const linksToAnchors = document.querySelectorAll('a[href^="#"]');
+
+linksToAnchors.forEach(each => (each.onclick = anchorLinkHandler));
